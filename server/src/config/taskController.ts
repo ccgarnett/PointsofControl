fcc2678493a699aeab9b54513413f339e42c8599
nybutler <nyriquebutler75@gmail.com>
2026-03-17 12:50:34 -0400

    begin refactoring for DailyChecklist, created Task model and controller

---
+++ server/src/config/taskController.ts
@@ -0,0 +1,7 @@
+import {Request, Response} from 'express';
+import Task from './Task';
+
+export const createTask = async (req: Request, res: Response) => {};
+export const readTask = async (req: Request, res: Response) => {};
+export const updateTask = async (req: Request, res: Response) => {};
+export const deleteTask = async (req: Request, res: Response) => {};
